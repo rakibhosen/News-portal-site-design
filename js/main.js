68c4a44082abb0445bd9e364c8668b44f3e5a8a6
Rakib Hosen <rakibhosen832@gmail.com>
2019-10-04 21:26:59 +0600

    final

--- js/main.js
+++ js/main.js
@@ -27,59 +27,10 @@ $(document).ready(function($){
 })
 
 // calender...........................................
- window.onload = function() {
-            Calendar.setup({
-              dateField     : 'date',
-              parentElement : 'calendar'
-            })
-          }
-// dropdown.............................................
-        const $dropdown = $(".dropdown");
-        const $dropdownToggle = $(".dropdown-toggle");
-        const $dropdownMenu = $(".dropdown-menu");
-        const showClass = "show";
-     
-    $(window).on("load resize", function() {
-      if (this.matchMedia("(min-width: 768px)").matches) {
-        $dropdown.hover(
-          function() {
-            const $this = $(this);
-            $this.addClass(showClass);
-            $this.find($dropdownToggle).attr("aria-expanded", "true");
-            $this.find($dropdownMenu).addClass(showClass);
-          },
-          function() {
-            const $this = $(this);
-            $this.removeClass(showClass);
-            $this.find($dropdownToggle).attr("aria-expanded", "false");
-            $this.find($dropdownMenu).removeClass(showClass);
-          }
-        );
-      } else {
-        $dropdown.off("mouseenter mouseleave");
-      }
-    });
 
-    // scrollbar.........................
-        //Get the button
-        var mybutton = document.getElementById("myBtn");
-    
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {scrollFunction()};
-    
-        function scrollFunction() {
-          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-          } else {
-            mybutton.style.display = "none";
-          }
-        }
-    
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-          document.body.scrollTop = 0;
-          document.documentElement.scrollTop = 0;
-        }
+
+
+
 
 
   
